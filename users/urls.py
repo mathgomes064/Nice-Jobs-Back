e74f5f0d736510078b21806ee450b6252dbddf13
Rodrigo Nunes <imrodrigon@gmail.com>
2023-01-04 11:17:17 -0400

    feat: update urls

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from rest_framework_simplejwt import views as jwt_views
 
 
 urlpatterns = [
+    path("login/", jwt_views.TokenObtainPairView.as_view()),
     path("users/", views.UserView.as_view()),
 ]
