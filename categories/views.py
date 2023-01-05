73f5ca3f0e0f7dc24d144336a4f90058ddb0be51
Gabriel Martins <lima.gabriel@sou.ufac.br>
2023-01-04 23:40:07 -0500

    feat: urls

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from .models import Category
+from .serializers import CategorySerializer
+from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class CategoryView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('category_id')
+        category_obj = get_object_or_404(Category, pk=category_id)
+        category = Category.objects.filter(category_id=category_obj)
+
+        return category
+
+    def perform_create(self, serializer):
+        serializer.save(user_id=self.request.user)
+
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
