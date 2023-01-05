c6eec9d4e4f0dcfdc677d2e9b7d932e3da76af5f
Rodrigo Nunes <imrodrigon@gmail.com>
2023-01-05 09:44:04 -0400

    feat: urls and view users

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,6 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path("login/", jwt_views.TokenObtainPairView.as_view()),
     path("users/", views.UserView.as_view()),
+    path("users/<uuid:user_id>/", views.UserDetailView.as_view()),
+    path("users/profile/", views.UserProfileView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,31 @@
-from rest_framework.generics import (
-    ListCreateAPIView,
-)
+from rest_framework.generics import ListCreateAPIView, UpdateAPIView, RetrieveAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from .models import User
 from .serializers import UserSerializer
+from .permissions import IsOwnerResource
 
 
 class UserView(ListCreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class UserDetailView(UpdateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsOwnerResource]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    lookup_url_kwarg = "user_id"
+
+
+class UserProfileView(RetrieveAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def get_object(self):
+        return self.request.user
