434974491338a135f8c416a0d54256635db2116b
Gabriel Martins <lima.gabriel@sou.ufac.br>
2023-01-04 23:40:00 -0500

    feat: views

--- categories/urls.py
+++ categories/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path("categories/", ), GET POST - ListCreateAPIView
-    # path("categories/<uuid:category_id>/",), GET - RetrieveApiView
+    path('', views.CategoryView.as_view(), name='categories'),
+    path('<int:category_id>', views.CategoryDetailView.as_view(),
+         name='category_detail'),
 ]
