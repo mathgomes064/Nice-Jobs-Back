6eb049e96bd125284f7a67b8096ab3b9f1150cbb
Gabriel Martins <lima.gabriel@sou.ufac.br>
2023-01-04 23:22:29 -0500

    feat: models

--- services/models.py
+++ services/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    class Meta:
+        ordering = ['id']
+        service_name = models.CharField(max_length=100, null=False)
+        is_active = models.BooleanField(default=True, null=False)
+        created_at = models.DateTimeField(auto_now_add=True, null=False)
+        updated_at = models.DateTimeField(auto_now=True, null=False)
+        description_id = models.ForeignKey(
+            "descriptions.Description", on_delete=models.CASCADE, null=False, related_name="service_description")
+        category_id = models.ForeignKey(
+            "categories.Category", on_delete=models.CASCADE, null=False, related_name="service_category")
+        user_id = models.ForeignKey(
+            "users.User", on_delete=models.CASCADE, null=False, related_name="service_user")
