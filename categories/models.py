972bb1a600edf3d92e3f3c8c4d77e009b3618689
P4ul0Silva <paulors43@outlook.com>
2023-01-07 18:52:14 -0300

    hotfix: add urgent categories choices

--- categories/models.py
+++ categories/models.py
@@ -4,8 +4,23 @@ import uuid
 
 
 class Category(models.Model):
+
+    CONSTRUCAO = "Construção civil"
+    MANUTENCAO = "Manutenção de Eletrônicos"
+    FRETE = "Frete"
+    DOMESTICOS = "Serviços Domésticos"
+    OUTROS = "Outros"
+
+    CATEGORY_CHOICES = (
+        (CONSTRUCAO, "Construção civil"),
+        (MANUTENCAO, "Manutenção de Eletrônicos"),
+        (FRETE, "Frete"),
+        (DOMESTICOS, "Serviços Domésticos"),
+        (OUTROS, "Outros"),
+    )
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default=OUTROS)
 
     class Meta:
         ordering = ["id"]
